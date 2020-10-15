490191ad3c983e84646905740df0896e471fd4e8
in2dam-b <in2dam-b@HZ317741.elorrieta.int>
2020-10-15 16:33:20 +0200

    PruebasLectura

--- testJunit/PruebasControlador/PruebaleerTxt.java
+++ testJunit/PruebasControlador/PruebaleerTxt.java
@@ -1,13 +1,25 @@
 package PruebasControlador;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.StreamCorruptedException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import modelo.Libro;
 
@@ -21,7 +33,7 @@ public class PruebaleerTxt {
 		try{
 			
 			int contadorEntradas = 0;
-			String sFichero = "libros";
+			String sFichero = "libros.txt";
 			
 			File fichero = new File(sFichero);
 			brFichero= new BufferedReader(new FileReader(fichero));
@@ -62,6 +74,106 @@ public class PruebaleerTxt {
 			System.out.println("\nError de E/S ");}
 		
 
+		return libros;
+	}
+	
+	public ArrayList<Libro> leerFicheroDat(ArrayList<Libro> pLibros) throws ParseException, IOException, ClassNotFoundException {
+		
+		ArrayList<Libro> libros = pLibros;
+		
+		try {
+			int contadorEntradas = 0;
+			
+			FileInputStream fi = new FileInputStream(new File("libros.dat"));
+			ObjectInputStream oi = new ObjectInputStream(fi);
+	   
+	        try {
+	            while (true) {
+
+	                Libro liburu = (Libro)oi.readObject();
+
+	                if (liburu != null) {
+	                    libros.add(liburu);
+	                    contadorEntradas++;
+	                }
+	                
+	            }
+        	} catch (EOFException eo) {
+	        	System.out.println("\nFIN DE LECTURA");
+        	} 
+	        catch (StreamCorruptedException x) {
+	        }
+
+			oi.close();
+			
+	        if (libros.isEmpty()){
+				System.out.println("\nNo se ha cargado ningun libro");
+			}
+			else {
+				System.out.println("\nSe ha(n) cargado en memoria " + contadorEntradas +" libro(s)");
+			}
+	        
+		} catch (FileNotFoundException e) {
+			System.out.println("\nNo se encuentra el fichero de carga");
+		}
+		
+		return libros;
+	
+	
+	
+	}
+	
+	
+	public ArrayList<Libro> leerFicheroXml(ArrayList<Libro> pLibros) throws ParseException, IOException, ClassNotFoundException {
+		
+		ArrayList<Libro> libros = pLibros;
+		
+		int contadorEntradas = 0;
+		
+		try {
+            File archivo = new File("libros.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+            Document document = documentBuilder.parse(archivo);
+            document.getDocumentElement().normalize();
+            NodeList listalibros = document.getElementsByTagName("LIBRO");
+            
+            
+            for (int temp = 0; temp < listalibros.getLength(); temp++) {
+                Node nodo = listalibros.item(temp);
+
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nodo;
+                    
+                    
+                    Libro liburu= new Libro();
+                    liburu.setTitulo(element.getAttribute("TITULO"));
+                    liburu.setEditorial(element.getAttribute("EDITORIAL"));
+                    liburu.setPaginas(Integer.parseInt(element.getAttribute("PAGINAS")));
+                    liburu.setAltura(Double.parseDouble(element.getAttribute("ALTURA")));
+                    liburu.setNotas(element.getAttribute("NOTAS"));
+                    liburu.setIsbn(element.getAttribute("ISBN"));
+                    liburu.setMaterias(element.getAttribute("MATERIAS"));
+                    
+                    libros.add(liburu);
+                    contadorEntradas++;
+                    
+                }
+            }
+            if (libros.isEmpty()){
+  				System.out.println("\nNo se ha cargado ningun libro");
+  			}
+  			else {
+  				System.out.println("\nSe ha(n) cargado en memoria " + contadorEntradas +" libro(s)");
+  			}
+            
+		} catch (FileNotFoundException e) {
+			System.out.println("\nNo se encuentra el fichero de carga");
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+		
 		return libros;
 	}
 }
--- testJunit/PruebasControlador/PruebaleerTxtTest.java
+++ testJunit/PruebasControlador/PruebaleerTxtTest.java
@@ -3,6 +3,7 @@ package PruebasControlador;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
@@ -18,6 +19,20 @@ class PruebaleerTxtTest {
 		ArrayList<Libro> pLibros = new ArrayList<Libro>();
 		BufferedReader prueba = null;
 		assertEquals(pLibros,  leertxt.leerFicheroTxt(pLibros, prueba));
+	}
+	
+	@Test
+	void test2() throws ParseException, InterruptedException, ClassNotFoundException, IOException {
+		PruebaleerTxt leerdat = new PruebaleerTxt();
+		ArrayList<Libro> pLibros = new ArrayList<Libro>();
+		assertEquals(pLibros,  leerdat.leerFicheroDat(pLibros));
+	}
+	
+	@Test
+	void test3() throws ParseException, InterruptedException, ClassNotFoundException, IOException {
+		PruebaleerTxt leerXml = new PruebaleerTxt();
+		ArrayList<Libro> pLibros = new ArrayList<Libro>();
+		assertEquals(pLibros,  leerXml.leerFicheroXml(pLibros));
 	}
 
 }
