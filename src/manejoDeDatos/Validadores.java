efe395a314686a9fc90109cd0f32dc0aed96b77c
JonV <jon.villanuevagi@elorrieta-errekamari.com>
2020-11-02 15:12:24 +0100

    Cambiado el validador de datos. Sprint 3

--- src/manejoDeDatos/Validadores.java
+++ src/manejoDeDatos/Validadores.java
@@ -1,23 +1,42 @@
 package manejoDeDatos;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Validadores {
 		
+	/***
+	 * esta funcion valida un nombre de archivo en funcion del patron de datos recibido, caracteres validos, longitud minima, longitud maxima
+	 * @param pDatoAValidar
+	 * @param pPatron
+	 * @return boolean
+	 */
+	
 	public static boolean validador(String pDatoAValidar, String pPatron) {
-		
-	    Pattern pat = Pattern.compile(pPatron);
-	
-	    Matcher mat = pat.matcher(pDatoAValidar);
-	
-	    if(!mat.matches()){
-			
-			return false;
-	    }		
-	    else {
-	    	return true;
-	    }
 
+		String[] lista= pPatron.split(";");
+		String Patron = lista[0];
+		int longitudmin = Integer.parseInt(lista[1]) ;
+		int longitudmax = Integer.parseInt(lista[2]) ;
+		
+		boolean valido = false;
+		
+		if(pDatoAValidar.length()>longitudmax || pDatoAValidar.length() < longitudmin ) {
+			return false;
+		}
+		
+		for (int x = 0 ; x<= pDatoAValidar.length()-1; x++) {
+			char letra= pDatoAValidar.charAt(x);
+			
+			for (int y = 0 ; y<= Patron.length()-1; y++) {
+				if(letra == Patron.charAt(y)) {
+					valido = true;
+				}
+			}
+			if(!valido) {
+				return false;
+			}
+			valido = false;
+		}	
+		return true;	
 	}
+
 }
